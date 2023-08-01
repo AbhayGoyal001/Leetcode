2d8fce17928ed0e42a445aaa1f4388e330188069
Abhay Goyal <abhaygoyalscience@gmail.com>
2023-08-01 08:44:18 +0530

    Time: 569 ms (15.34%), Space: 44.2 MB (66.98%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> matrix = new ArrayList<>();
+        List<Integer> row = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+        permutelist(list, row, matrix);
+        return matrix;
+    }
+
+    public static void permutelist(List<Integer> list, List<Integer> row, List<List<Integer>> matrix) {
+        if (list.size() == 0) {
+
+            if(matrix.contains(row)){
+                return;
+            }
+            matrix.add(new ArrayList<>(row)); 
+            return;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            List<Integer> copy = new ArrayList<>(list);
+            copy.remove(i);
+            row.add(list.get(i));
+            permutelist(copy, row, matrix);
+            row.remove(row.size() - 1);
+        }
+    }
+}
