298836a8789720494062a5fd183bcfce34251bea
Abhay Goyal <abhaygoyalscience@gmail.com>
2025-02-19 14:09:33 +0530

    Time: 24 ms (23.23%), Space: 45.3 MB (28.47%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String getHappyString(int n, int k) {
+        List<String> list = new ArrayList<String>();
+        String ans ="";
+        backtrack(n,"",list);
+        return (k>list.size()) ? "" : list.get(k-1);
+    }
+    public static void backtrack(int n,String ans,List<String> list){
+        if(ans.length()==n) {
+            list.add(ans);
+            return;
+        }
+
+        char[] c =  {'a','b','c'};
+        for(char ch :c ){
+            if(ans.isEmpty()||ans.charAt(ans.length()-1)!=ch){
+                backtrack(n,ans+ch,list);
+            }
+        }
+
+    
+    }
+}
