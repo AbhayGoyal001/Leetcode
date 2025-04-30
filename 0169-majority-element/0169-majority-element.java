366beaa119aa920a9376a36b5b02d7b9b859101f
Abhay Goyal <abhaygoyalscience@gmail.com>
2025-04-30 10:07:17 +0530

    Time: 1 ms (99.87%), Space: 54 MB (8.81%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,12 +3,12 @@ class Solution {
         int count = 0;
         int target = -1;
         for(int i =0;i<nums.length;i++){
-            if(count == 0){
-                target = nums[i];
-                count=1;
+            if(count == 0){// If majority element exist then it is in starting of the array of starting element is the target we have to check and starting elements  is wrong target  then automatically count will be zero there is a decrement in below
+                target = nums[i]; //Initialize the value and found the target
+                count=1; // Increment Occurence 
             }
-            else if(target==nums[i]) count++;
-            else count--;
+            else if(target==nums[i]) count++; // If present then increase the counter
+            else count--;// Either decrease the counter 
         }
         return target;
     }
