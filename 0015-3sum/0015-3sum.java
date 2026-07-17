1b583abbff5db0edbcac8e1c99e74bd67e3ce988
Abhay Goyal <abhaygoyalscience@gmail.com>
2026-07-17 22:35:33 +0530

    Time: 37 ms (24.56%), Space: 59.2 MB (45.83%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,48 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // Sort the array
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate elements for i
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int j = i + 1;
-            int k = nums.length - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum == 0) {
-                    // Found a triplet with zero sum
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    // Skip duplicate elements for j
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-
-                    // Skip duplicate elements for k
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-
-                    // Move the pointers
-                    j++;
-                    k--;
-                } else if (sum < 0) {
-                    // Sum is less than zero, increment j to increase the sum
-                    j++;
-                } else {
-                    // Sum is greater than zero, decrement k to decrease the sum
-                    k--;
+        for(int i =0 ;i<nums.length-2;i++){
+            if(i>0 && nums[i]== nums[i-1]) continue;
+            int start = i+1;
+            int end = nums.length-1;
+            while(start<end){
+                if(nums[i]+nums[start]+nums[end]==0){
+                    list.add(Arrays.asList(nums[i],nums[start],nums[end]));
+                    start++;
+                    end--;
+                    while(start<end && nums[start]== nums[start-1]) start++;
+                    while(start<end && nums[end]== nums[end+1]) end--;
                 }
+                else if(nums[i]+nums[start]+nums[end]>0) end--;
+                else start++;
             }
         }
-        return ans;
+        return list;
     }
 }
